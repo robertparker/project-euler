11fe25e5db791e842c21c237199236bfe30c86bf
Rohit Parulkar <rohit.parulkar@livingsocial.com>
2014-02-25 23:54:25 -0500

    solver #143386

--- 010.rb
+++ 010.rb
@@ -1,17 +1,5 @@
 require_relative "resources/resources"
 
-# primes = []
-# not_primes = []
-
-# max = 2000000
-# (2..max).each do |num|
-# 	p num
-# 	primes << num if is_prime?(num)
-# end
-
-# p primes.inject(&:+)
-
-
 def get_all_primes_up_to(max)
 	primes = [2]
 	(2..max).each do |num|
